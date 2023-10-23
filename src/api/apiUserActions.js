09e7381325c1e9d14d8806c1c4f5674907860a95
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-10-23 16:32:42 +0500

    useractions

--- src/api/apiUserActions.js
+++ src/api/apiUserActions.js
@@ -1,1 +1,12 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { putDislikeTrack, putLikeTrack } from "../store/actions";
+import baseQueryWithReauth from "./baseQueruWithReauth";
 
+const TRACKS_MARKER = "Tracks";
+
+export const userActions = createApi({
+  reducerPath: "userActions",
+  baseQuery: baseQueryWithReauth,
+
+  
+})
