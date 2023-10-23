2a106a5e671965ef395666ee012fefa4602859d7
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-10-23 17:27:57 +0500

    получение треков, лайки/диз

--- src/api/apiUserActions.js
+++ src/api/apiUserActions.js
@@ -31,7 +31,39 @@ export const userActions = createApi({
           ],
         }));
       },
-})
-
-
-})
+      providesTags: () => [TRACKS_MARKER],
+    }),
+    getCategory: builder.query({
+      query: ({ id }) => ({
+        url: `/catalog/selection/${id}/`,
+      }),
+      providesTags: () => [TRACKS_MARKER],
+    }),
+    likeTrack: builder.mutation({
+      query: ({ id }) => ({
+        url: `/catalog/track/${id}/favorite/`,
+        method: "POST",
+      }),
+      invalidatesTags: [TRACKS_MARKER],
+      async onQueryStarted(args, { dispatch, getState, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(putLikeTrack({ id: args.id, auth: getState().auth }));
+        } catch (error) {}
+      },
+    }),
+    dislikeTrack: builder.mutation({
+      query: ({ id }) => ({
+        url: `/catalog/track/${id}/favorite/`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [TRACKS_MARKER],
+      async onQueryStarted(args, { dispatch, getState, queryFulfilled }) {
+        try {
+          await queryFulfilled;
+          dispatch(putDislikeTrack({ id: args.id, auth: getState().auth }));
+        } catch (error) {}
+      },
+    }),
+  }),
+});
