128b283e5d623c72839153aeaf5fe0e86afcf45c
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-10-23 17:30:59 +0500

    изм компонент аудиоплеер

--- src/components/AudioPlayer/AudioPlayer.jsx
+++ src/components/AudioPlayer/AudioPlayer.jsx
@@ -1,166 +1,318 @@
-import { useRef, useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import { useCallback, useEffect, useRef, useState } from "react";
+import "./style";
 import * as S from "./style";
-import { SkeletonPlayBar } from "../Tracks/style";
-import { AudioPlayerIcons } from "../AudioPlayerIcons/AudioPlayerIcons";
-import { AudioVolume } from "../AudioVolume/AudioVolume";
-import { BarPlayerProgress } from "../AudioPlayerProgress/AudioPlayerProgress";
+import { useDispatch, useSelector } from "react-redux";
 import {
-  isPlayingSelector,
-  allTracksSelector,
-  indexTrackSelector,
-  shuffleSelector,
-  shuffleTracks,
-} from "../../store/selectors/index";
-import {
-  setIsPlaying,
+  pauseTrack,
+  playTrack,
   nextTrack,
   prevTrack,
-  toggleShuffleTracks,
-} from "../../store/actions/index";
+  shuffleTracks,
+} from "../../store/actions";
+import { useAuthSelector } from "../../auth.js";
+import {
+  useDislikeTrackMutation,
+  useLikeTrackMutation,
+} from "../../api/apiUserActions";
 
-export function AudioPlayer({ isLoading, currentTrack }) {
-  const dispatch = useDispatch();
-  const isPlaying = useSelector(isPlayingSelector);
-  const shuffle = useSelector(shuffleSelector);
-  const shuffleAllTracks = useSelector(shuffleTracks);
-  const tracks = useSelector(allTracksSelector);
-  const [timeProgress, setTimeProgress] = useState(0);
-  const [duration, setDuration] = useState(0);
+function durationTime(seconds) {
+  return (
+    Math.floor(Math.round(seconds) / 60)
+      .toString()
+      .padStart(2, "0") +
+    ":" +
+    (Math.round(seconds) % 60).toString().padStart(2, "0")
+  );
+}
+
+export default function AudioPlayer({ track }) {
   const audioRef = useRef(null);
-  const indexTrack = useSelector(indexTrackSelector);
-  const [repeatTrack, setRepeatTrack] = useState(false);
+  const auth = useAuthSelector();
+  const [isRepeat, setisRepeat] = useState(false);
+  const [currentTime, setCurrentTime] = useState(0);
+  const [duration, setDuration] = useState(0);
+  const [volume, setVolume] = useState(1);
+  const dispatch = useDispatch();
+  const { shuffled, playing } = useSelector((store) => store.audioplayer);
 
-  const handleStart = () => {
-    audioRef.current.play();
-    dispatch(setIsPlaying(true));
-  };
-  const handleStop = () => {
-    audioRef.current.pause();
-    dispatch(setIsPlaying(false));
+  const url = track.track_file;
+  const name = track.name;
+  const artist = track.author;
+
+  const isTrackLiked = !!(track.stared_user ?? []).find(
+    ({ id }) => id === auth.id,
+  );
+
+  const play = useCallback(() => {
+    dispatch(playTrack());
+  }, [dispatch]);
+
+  const pause = useCallback(() => {
+    dispatch(pauseTrack());
+  }, [dispatch]);
+
+  const toggleRepeat = () => {
+    if (isRepeat) {
+      setisRepeat(false);
+      audioRef.current.loop = false;
+    } else {
+      setisRepeat(true);
+      audioRef.current.loop = true;
+    }
   };
 
-  const togglePlay = isPlaying ? handleStop : handleStart;
-  const arrayTracksAll = shuffle ? shuffleAllTracks : tracks;
+  const handleVolumeChange = (event) => {
+    const newVolume = event.target.value;
+    setVolume(newVolume);
+    audioRef.current.volume = newVolume;
+  };
+
+  const handleSeek = (newTime) => {
+    audioRef.current.currentTime = newTime;
+    play();
+  };
+
+  const handlePrevClick = () => {
+    if (currentTime > 4) {
+      handleSeek(0);
+    } else {
+      dispatch(prevTrack());
+    }
+  };
+
+  const handleNextClick = useCallback(() => {
+    dispatch(nextTrack());
+  }, [dispatch]);
+
+  const [like, { error: likeError }] = useLikeTrackMutation();
+  const [dislike, { error: dislikeError }] = useDislikeTrackMutation();
+
+  const likeCurrentTrack = () => {
+    like({
+      id: track.id,
+    });
+  };
+  const dislikeCurrentTrack = () => {
+    dislike({
+      id: track.id,
+    });
+  };
 
   useEffect(() => {
-    handleStart();
-    audioRef.current.onended = () => {
-      dispatch(nextTrack(arrayTracksAll[indexTrack + 1], indexTrack + 1));
+    const ref = audioRef.current;
+
+    setisRepeat(ref.loop);
+    setVolume(ref.volume);
+
+    const handleTimeUpdateEvent = (event) => {
+      if (ref.currentTime && ref.duration) {
+        setCurrentTime(ref.currentTime);
+        setDuration(ref.duration);
+      } else {
+        setCurrentTime(0);
+        setDuration(0);
+      }
     };
-  }, [currentTrack]);
 
-  const onLoadedMetadata = () => {
-    setDuration(audioRef.current.duration);
-  };
-  const onTimeUpdate = () => {
-    setTimeProgress(audioRef.current.currentTime);
-  };
+    ref.addEventListener("timeupdate", handleTimeUpdateEvent);
 
-  const toggleTrackRepeat = () => {
-    audioRef.current.loop = !repeatTrack;
-    setRepeatTrack(!repeatTrack);
-  };
-  const toggleCurrentTrack = (alt) => {
-    if (alt === "next") {
-      const indexNextTrack = indexTrack + 1;
-      return dispatch(
-        nextTrack(arrayTracksAll[indexNextTrack], indexNextTrack),
-      );
+    return () => {
+      ref.removeEventListener("timeupdate", handleTimeUpdateEvent);
+    };
+  }, []);
+
+  useEffect(() => {
+    const handleSpacePress = (event) => {
+      if (event.key === " ") {
+        event.preventDefault();
+        if (playing) {
+          pause();
+        } else {
+          play();
+        }
+      }
+    };
+
+    document.addEventListener("keypress", handleSpacePress);
+
+    return () => {
+      document.removeEventListener("keypress", handleSpacePress);
+    };
+  }, [play, pause, playing]);
+
+  useEffect(() => {
+    const ref = audioRef.current;
+    if (playing) {
+      ref.play();
+    } else {
+      ref.pause();
     }
-    if (alt === "prev" && indexTrack > 0) {
-      const indexPredTrack = indexTrack - 1;
-      return dispatch(
-        prevTrack(arrayTracksAll[indexPredTrack], indexPredTrack),
-      );
-    }
-  };
+  }, [playing, url]);
+
+  useEffect(() => {
+    const ref = audioRef.current;
+
+    ref.addEventListener("ended", handleNextClick);
+    return () => {
+      ref.removeEventListener("ended", handleNextClick);
+    };
+  }, [handleNextClick]);
+
+  const error = likeError ?? dislikeError ?? null;
+
+  if (error) {
+    alert(`Ошибка лайка: ${error.message}`);
+  }
 
   return (
     <S.Bar>
-      <audio
-        src={currentTrack.track_file}
-        ref={audioRef}
-        onTimeUpdate={onTimeUpdate}
-        onLoadedMetadata={onLoadedMetadata}
-      />
+      <S.HiddenAudio ref={audioRef} controls src={url} />
+
       <S.BarContent>
-        <BarPlayerProgress
-          duration={duration}
-          timeProgress={timeProgress}
-          audioRef={audioRef}
-        />
-        <S.BarPlayerBlock>
+        <S.ProgressInfoContainer>
+          <S.ProgressInfoTiming>
+            {durationTime(currentTime)} / {durationTime(duration)}
+          </S.ProgressInfoTiming>
+        </S.ProgressInfoContainer>
+        <S.ProgressInput
+          type="range"
+          min={0}
+          max={duration}
+          value={currentTime}
+          step={0.01}
+          onChange={(event) => handleSeek(event.target.value)}
+        ></S.ProgressInput>
+        <S.PlayerBlock>
           <S.BarPlayer>
             <S.PlayerControls>
-              <AudioPlayerIcons
-                alt="prev"
-                click={() => {
-                  toggleCurrentTrack("prev");
-                }}
-              />
-              <AudioPlayerIcons
-                alt={isPlaying ? "pause" : "play"}
-                click={togglePlay}
-              />
-              <AudioPlayerIcons
-                alt="next"
-                click={() => {
-                  toggleCurrentTrack("next");
-                }}
-              />
-              <AudioPlayerIcons alt="repeat" click={toggleTrackRepeat} />
-              <AudioPlayerIcons
-                alt="shuffle"
-                click={() => {
-                  dispatch(toggleShuffleTracks(!shuffle));
-                }}
-              />
+              <S.PlayerControlWithRightMargin>
+                <S.PlayerSvgPrev alt="prev" onClick={handlePrevClick}>
+                  <svg>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-prev"></use>
+                  </svg>
+                </S.PlayerSvgPrev>
+              </S.PlayerControlWithRightMargin>
+              <S.PlayerControlWithRightMargin className="_btn">
+                {playing ? (
+                  <S.PlayerSvgPlay onClick={pause} alt="play">
+                    <svg
+                      width="15"
+                      height="19"
+                      viewBox="0 0 15 19"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <rect width="5" height="19" fill="#D9D9D9" />
+                      <rect x="10" width="5" height="19" fill="#D9D9D9" />
+                    </svg>
+                  </S.PlayerSvgPlay>
+                ) : (
+                  <S.PlayerSvgPlay onClick={play} alt="play">
+                    <svg>
+                      <use xlinkHref="/img/icon/sprite.svg#icon-play"></use>
+                    </svg>
+                  </S.PlayerSvgPlay>
+                )}
+              </S.PlayerControlWithRightMargin>
+              <S.PlayerNextIcon>
+                <S.PlayerSvgNext alt="next" onClick={handleNextClick}>
+                  <svg>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-next"></use>
+                  </svg>
+                </S.PlayerSvgNext>
+              </S.PlayerNextIcon>
+              <S.PlayerRepeatIcon className="_btn-icon" onClick={toggleRepeat}>
+                <S.PlayerSvgRepeat alt="repeat" $active={isRepeat}>
+                  <svg>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-repeat"></use>
+                  </svg>
+                </S.PlayerSvgRepeat>
+              </S.PlayerRepeatIcon>
+              <S.PlayerShuffleIcon className="_btn-icon">
+                <S.PlayerSvgShuffle
+                  alt="shuffle"
+                  $active={shuffled}
+                  onClick={() => {
+                    dispatch(shuffleTracks());
+                  }}
+                >
+                  <svg>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-shuffle"></use>
+                  </svg>
+                </S.PlayerSvgShuffle>
+              </S.PlayerShuffleIcon>
             </S.PlayerControls>
-            <S.PlayerTrackPlay>
-              <S.TrackPlayContain>
+
+            <S.TrackPlay>
+              <S.TrackContain>
                 <S.TrackPlayImage>
                   <S.TrackPlaySvg alt="music">
-                    <use xlinkHref="img/icon/sprite.svg#icon-note" />
+                    <svg>
+                      <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
+                    </svg>
                   </S.TrackPlaySvg>
                 </S.TrackPlayImage>
+                <S.TrackPlayAuthor>
+                  <S.TrackPlayAuthorLink>
+                    <span>{name}</span>
+                  </S.TrackPlayAuthorLink>
+                </S.TrackPlayAuthor>
+                <S.TrackPlayAlbum>
+                  <S.TrackPlayAlbumLink>
+                    <span>{artist}</span>
+                  </S.TrackPlayAlbumLink>
+                </S.TrackPlayAlbum>
+              </S.TrackContain>
 
-                {isLoading ? (
-                  <S.TrackPlayAuthor>
-                    <S.TrackPlayAuthorLink href="http://">
-                      {currentTrack.name}
-                    </S.TrackPlayAuthorLink>
-                  </S.TrackPlayAuthor>
+              <S.TrackPlayLikeDis>
+                {!isTrackLiked ? (
+                  <S.TrackLike className="_btn-icon" onClick={likeCurrentTrack}>
+                    <S.TrackLikeSvg alt="like">
+                      <svg>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                      </svg>
+                    </S.TrackLikeSvg>
+                  </S.TrackLike>
                 ) : (
-                  <SkeletonPlayBar> </SkeletonPlayBar>
+                  <S.TrackDislike
+                    className="_btn-icon"
+                    onClick={dislikeCurrentTrack}
+                  >
+                    <S.TrackLikeSvg alt="dislike">
+                      <svg>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-dislike"></use>
+                      </svg>
+                    </S.TrackLikeSvg>
+                  </S.TrackDislike>
                 )}
-
-                {isLoading ? (
-                  <S.TrackPlayAlbum>
-                    <S.TrackPlayAlbumLink href="http://">
-                      {currentTrack.author}
-                    </S.TrackPlayAlbumLink>
-                  </S.TrackPlayAlbum>
-                ) : (
-                  <SkeletonPlayBar> </SkeletonPlayBar>
-                )}
-              </S.TrackPlayContain>
-              <S.TrackLikeDis>
-                <S.TrackLike>
-                  <S.TrackLikeSvg alt="like">
-                    <use xlinkHref="img/icon/sprite.svg#icon-like" />
-                  </S.TrackLikeSvg>
-                </S.TrackLike>
-                <S.TrackDislike>
-                  <S.TrackDislikeSvg alt="dislike">
-                    <use xlinkHref="img/icon/sprite.svg#icon-dislike" />
-                  </S.TrackDislikeSvg>
-                </S.TrackDislike>
-              </S.TrackLikeDis>
-            </S.PlayerTrackPlay>
+              </S.TrackPlayLikeDis>
+            </S.TrackPlay>
           </S.BarPlayer>
-          <AudioVolume audioRef={audioRef} />
-        </S.BarPlayerBlock>
+          <S.BarVolumeBlock>
+            <S.VolumeContent>
+              <S.VolumeImage>
+                <S.VolumeSvg alt="volume">
+                  <svg>
+                    <use xlinkHref="/img/icon/sprite.svg#icon-volume"></use>
+                  </svg>
+                </S.VolumeSvg>
+              </S.VolumeImage>
+              <S.VolumeProgress className="_btn">
+                <S.ProgressInput
+                  className="_btn"
+                  type="range"
+                  name="range"
+                  value={volume}
+                  max={1}
+                  min={0}
+                  step={0.01}
+                  onChange={handleVolumeChange}
+                  $color="white"
+                />
+              </S.VolumeProgress>
+            </S.VolumeContent>
+          </S.BarVolumeBlock>
+        </S.PlayerBlock>
       </S.BarContent>
     </S.Bar>
   );
