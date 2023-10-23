1a5906a6cc79eb6b8fcd00419a3a8a9c9c1a7869
Olesya Shinka <129954640+ol-shinka@users.noreply.github.com>
2023-10-23 15:59:38 +0500

    +

--- src/api/baseQueruWithReauth.js
+++ src/api/baseQueruWithReauth.js
@@ -1,4 +1,18 @@
 import { setAuthUser } from "../store/slices/UserLS";
 import { fetchBaseQuery } from "@reduxjs/toolkit/query";
 
-
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+  const baseQuery = fetchBaseQuery({
+    baseUrl: "https://skypro-music-api.skyeng.tech",
+    prepareHeaders: (headers, { getState }) => {
+      const token = getState().auth.access;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  });
+  const result = await baseQuery(args, api, extraOptions);
+  if (result?.error?.status !== 401) {
+    return result;
+  }
